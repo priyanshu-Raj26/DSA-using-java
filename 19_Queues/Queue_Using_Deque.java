36c1e15fea9f71b42a37140daabd1aa5906bdf89
Priyanshu Raj <er.priyansu@gmail.com>
2025-07-25 17:48:11 +0530

    Add files via upload

---
+++ 19_Queues/Queue_Using_Deque.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Queue_Using_Deque {
+    
+    static class Queue {
+        Deque<Integer> deque = new LinkedList<>();
+        
+        //add
+        public void add(int data) {
+            deque.addLast(data);
+        }
+
+        //remove
+        public int remove() {
+            return deque.removeFirst();
+        }
+
+        //peek
+        public int peek() {
+            return deque.getFirst();
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("peek = " + q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+    } 
+}
