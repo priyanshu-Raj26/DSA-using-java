c9fcf2741333a239fafd52a646146ddb6f4617f2
Priyanshu Raj <er.priyansu@gmail.com>
2025-08-25 19:39:00 +0530

    Add files via upload

---
+++ 23_Binary_Tree/Transform_to_sum_Tree.java
@@ -0,0 +1,56 @@
+public class Transform_to_sum_Tree {
+    
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
+    public static int Transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftchild = Transform(root.left);
+        int rightchild = Transform(root.right);
+
+        int data = root.data; 
+
+        int newleft = root.left == null ? 0 : root.left.data;
+        int newright = root.right == null ? 0 : root.right.data;
+
+        root.data = newleft + leftchild + newright + rightchild;
+
+        return data;
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+         Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Transform(root);
+        preorder(root);
+    }
+}
