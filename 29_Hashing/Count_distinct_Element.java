f8fe1898e9d69ab0809e5ab28c318bc033124fd6
Priyanshu Raj <er.priyansu@gmail.com>
2025-09-18 00:58:08 +0530

    Add files via upload

---
+++ 29_Hashing/Count_distinct_Element.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class Count_distinct_Element {
+    
+    public static void main(String[] args) {
+        int num[] = {4, 3, 2, 5, 6, 7, 3, 4, 2, 1};
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < num.length; i++) { //O(n)
+            set.add(num[i]);
+        }
+
+        System.out.println("ans = " + set.size());
+
+    }
+}
