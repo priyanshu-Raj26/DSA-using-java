dcf09fb96ea1a1cec4edb69b9bf9a54d5666c917
Priyanshu Raj <er.priyansu@gmail.com>
2025-10-04 23:55:00 +0530

    Add files via upload

---
+++ 31_Tries/Build_a_Trie.java
@@ -0,0 +1,51 @@
+public class Build_a_Trie {
+
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) { // O(L)
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static boolean Search(String key) { // O(L)
+        Node curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            int idx = key.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "the", "a", "there", "their", "any", "thee" };
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+
+        System.out.println(Search("there"));
+        System.out.println(Search("thor"));
+    }
+}
