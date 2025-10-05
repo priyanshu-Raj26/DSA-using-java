c8b96726b4bd7bb9d78c54495d24b1535d857ac7
Priyanshu Raj <er.priyansu@gmail.com>
2025-10-06 02:08:53 +0530

    Add files via upload

---
+++ 31_Tries/Prefix_Problem.java
@@ -0,0 +1,68 @@
+public class Prefix_Problem {
+
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        Node() {
+            for (int i = 0; i < children.length; i++) {
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) { // O(L)
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static String findUniquePrefix(String word) {
+        Node curr = root;
+        StringBuilder prefix = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            prefix.append(word.charAt(i));
+            curr = curr.children[idx];
+
+            if (curr.freq == 1) {
+                break; // Found unique prefix
+            }
+        }
+
+        return prefix.toString();
+    }
+
+    public static void printPrefix(String arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+            String prefix = findUniquePrefix(word);
+            System.out.println(word + " -> " + prefix);
+        }
+    }
+
+    public static void main(String[] args) {
+        String arr[] = { "zebra", "dog", "duck", "dove" };
+
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+
+        printPrefix(arr);
+
+    }
+}
